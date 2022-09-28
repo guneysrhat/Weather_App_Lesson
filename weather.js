7a2e85f010c824bba3d44fcf68b96376ad7b72c2
guneysrhat <guneysrhat@gmail.com>
2022-09-28 20:24:54 +0300

    api

--- weather.js
+++ weather.js
@@ -2,3 +2,12 @@ const form = document.querySelector("section.top-banner form");
 const input = document.querySelector(".container input");
 const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
+
+localStorage.getItem(
+  "tokenkey",
+  "ZXaqXBz5mOOXEyTuqjldnTwDQXH8YKzqhgUxv7tmSkvzyjz2e4AB8FeUbj1fEesG"
+);
+// localStorage.setItem(
+//   "tokenKeyEncrypted",
+//   EncryptStringAES("09f64283148ea6369c6273dbf953a101")
+// );
