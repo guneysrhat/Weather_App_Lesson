5a41f232dee37fd13dd44839581c02fc1a38877d
guneysrhat <guneysrhat@gmail.com>
2022-09-28 22:43:40 +0300

    capturung

--- weather.js
+++ weather.js
@@ -20,33 +20,67 @@ form.addEventListener("submit", (event) => {
 const getWeatherDataFromApi = async () => {
   //alert("http request is gone!");
   const tokenKey = DecryptStringAES(localStorage.getItem("tokenKey"));
-  alert(tokenKey);
+  //alert(tokenKey);
   const inputValue = input.value;
   const units = "metric";
   const lang = "tr";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
+  try {
+    // const response = await fetch(url).then((response) => response.json());
+    const response = await axios(url);
+    console.log(response);
+    const { main, sys, weather, name } = response.data;
+    const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+    const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
 
-  const response = await fetch(url).then((response) => response.json());
-  console.log(response);
+    const cityNameSpans = list.querySelectorAll(".city span");
+    const cityNameSpansArray = Array.from(cityNameSpans);
+    if (cityNameSpansArray.length > 0) {
+      const filteredArray = cityNameSpansArray.filter(
+        (span) => span.innerText == name
+      );
+      if (filteredArray.length > 0) {
+        msg.innerText = `You already know the weather for ${name}, Please search for another city 😉`;
+        setTimeout(() => {
+          msg.innerText = "";
+        }, 5000);
+        form.reset();
+        return;
+      }
+    }
+    console.log(cityNameSpans);
+    const createdLi = document.createElement("li");
+    createdLi.classList.add("city");
+    createdLi.innerHTML = `<h2 class="city-name" data-name="${name}, ${
+      sys.country
+    }">
+                              <span>${name}</span>
+                              <sup>${sys.country}</sup>
+                          </h2>
+                          <div class="city-temp">${Math.round(
+                            main.temp
+                          )}<sup>°C</sup></div>
+                          <figure>
+                              <img class="city-icon" src="${iconUrl}">
+                              <figcaption>${weather[0].description}
+                              </figcaption>
+                          </figure>`;
+    //* append vs prepend
+    list.prepend(createdLi);
 
-  const { main, sys, weather, name } = response;
+    //* Capturing
+    createdLi.addEventListener("click", (e) => {
+      if (e.target.tagName == "IMG") {
+        e.target.src = e.target.src == icaonUrl ? iconUUrlAWS : iconUrl;
+      }
+    });
 
-  const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
-
-  const createdLi = document.createElement("li");
-  createdLi.classList.add("city");
-  createdLi.innerHTML = `<h2 class="city-name" data-name="${name}, ${
-    sys.country
-  }">
-                            <span>${name}</span>
-                            <sup>${sys.country}</sup>
-                        </h2>
-                        <div class="city-temp">${Math.round(
-                          main.temp
-                        )}<sup>°C</sup></div>
-                        <figure>
-                            <img class="city-icon" src="${iconUrlAWS}">
-                            <figcaption>${weather[0].description}
-                            </figcaption>
-                        </figure>`;
+    form.reset();
+  } catch (error) {
+    msg.innerText = `404 (City Not Found!)`;
+    setTimeout(() => {
+      msg.innerText = "";
+    }, 5000);
+    form.reset();
+  }
 };
