e504a89e4034666065536fa61cdfecc6e49449a4
guneysrhat <guneysrhat@gmail.com>
2022-09-28 20:48:10 +0300

    tokenkey

--- weather.js
+++ weather.js
@@ -3,7 +3,7 @@ const input = document.querySelector(".container input");
 const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
 
-localStorage.getItem(
+localStorage.setItem(
   "tokenkey",
   "ZXaqXBz5mOOXEyTuqjldnTwDQXH8YKzqhgUxv7tmSkvzyjz2e4AB8FeUbj1fEesG"
 );
@@ -11,3 +11,19 @@ localStorage.getItem(
 //   "tokenKeyEncrypted",
 //   EncryptStringAES("09f64283148ea6369c6273dbf953a101")
 // );
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWeatherDateFormApi();
+});
+
+//* Get api Func.(http method == Verbs)
+
+const getWeatherDateFormApi = () => {
+  alert("http request is gone!");
+  const tokenKey = DecryptStringAES(
+    "ZXaqXBz5mOOXEyTuqjldnTwDQXH8YKzqhgUxv7tmSkvzyjz2e4AB8FeUbj1fEesG"
+  );
+  alert(tokenKey);
+  const inputValue = input.value;
+  const url 
+};
