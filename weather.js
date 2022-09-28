cb7352e14023922d80fe44d3cbdcdfe568b39eeb
guneysrhat <guneysrhat@gmail.com>
2022-09-28 21:35:50 +0300

    inner html

--- weather.js
+++ weather.js
@@ -4,26 +4,49 @@ const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
 
 localStorage.setItem(
-  "tokenkey",
+  "tokenKey",
   "ZXaqXBz5mOOXEyTuqjldnTwDQXH8YKzqhgUxv7tmSkvzyjz2e4AB8FeUbj1fEesG"
 );
 // localStorage.setItem(
 //   "tokenKeyEncrypted",
 //   EncryptStringAES("09f64283148ea6369c6273dbf953a101")
 // );
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  getWeatherDateFormApi();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getWeatherDataFromApi();
 });
 
 //* Get api Func.(http method == Verbs)
-
-const getWeatherDateFormApi = () => {
-  alert("http request is gone!");
-  const tokenKey = DecryptStringAES(
-    "ZXaqXBz5mOOXEyTuqjldnTwDQXH8YKzqhgUxv7tmSkvzyjz2e4AB8FeUbj1fEesG"
-  );
+const getWeatherDataFromApi = async () => {
+  //alert("http request is gone!");
+  const tokenKey = DecryptStringAES(localStorage.getItem("tokenKey"));
   alert(tokenKey);
   const inputValue = input.value;
-  const url 
+  const units = "metric";
+  const lang = "tr";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
+
+  const response = await fetch(url).then((response) => response.json());
+  console.log(response);
+
+  const { main, sys, weather, name } = response;
+
+  const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+
+  const createdLi = document.createElement("li");
+  createdLi.classList.add("city");
+  createdLi.innerHTML = `<h2 class="city-name" data-name="${name}, ${
+    sys.country
+  }">
+                            <span>${name}</span>
+                            <sup>${sys.country}</sup>
+                        </h2>
+                        <div class="city-temp">${Math.round(
+                          main.temp
+                        )}<sup>°C</sup></div>
+                        <figure>
+                            <img class="city-icon" src="${iconUrlAWS}">
+                            <figcaption>${weather[0].description}
+                            </figcaption>
+                        </figure>`;
 };
